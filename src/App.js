aaf73fd3b366a4c3796d8e6cb35f2fda61b65b2b
unknown <jhunjhunwalanakul@gmail.com>
2020-07-19 03:38:27 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( <
+    div className = "App" >
+
+
+    <
+    div className = "digicard" >
+
+    <div className = "pback"
+    // style = {
+    //   {
+    //     backgroundImage: "url(https://www.websys.co.in/ban2019/banners6.jpg)"
+    //   }
+    // }
+     >
+
+    </div> 
+    
+    <div className = "details" >
+
+    <center >
+    <div className = "profile-box" >
+    <div className = "container" >
+      <div className = "profile"
+      style = {
+        {
+          backgroundImage: "url(https://scontent-sjc3-1.cdninstagram.com/v/t51.2885-19/s150x150/75296733_441851223392219_5753152136586199040_n.jpg?_nc_ht=scontent-sjc3-1.cdninstagram.com&_nc_ohc=t5dPmS8B510AX9iMo4t&oh=b99f236f1d08014f76ef6570a261d06b&oe=5F3987C4)"
+        }
+      } 
+      >
+
+      </div>
+
+      <h1> Nakul Jhunjhunwala </h1> <p> Web Developer </p> 
+    </div>
+
+    </div> 
+    </center> 
+    
+    <div className="info">
+    <p className="titles">Contact :-</p>
+
+    <div className="info-container">
+
+
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+
+      </div>
+      <p>text</p>
+      </div>
+
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+
+      </div>
+      <p>text</p>
+      </div>
+
+
+    </div>
+
+    <p className="titles">Profile :-</p>
+    <div className="info-container">
+
+
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      <div className="button-box">
+      <div className="button">
+      
+      </div>
+      <p>text</p>
+      </div>
+      
+      </div>
+    
+
+    </div>
+
+    
+   
+    
+    
+    
+    </div>
+
+
+
+    </div>
+
+
+
     </div>
   );
 }
